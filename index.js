8ab1cc8a0fd2ed142154f8cefd47c2ac0766ab06
Federico <fedee.unates.2001@gmail.com>
2024-03-19 21:57:35 -0300

    GET todas las notas desde la BD

--- index.js
+++ index.js
@@ -10,33 +10,35 @@ const Note = require("./models/Note");
 app.use(cors());
 app.use(express.json());
 
-let notes = [
-  {
-    id: 1,
-    content: "estudiar node js y express",
-    date: "2024-03-14T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 3,
-    content: "estudiar nest js y MongoDb",
-    date: "2024-03-14T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "estudiar websockets",
-    date: "2024-03-14T17:30:31.098Z",
-    important: true,
-  },
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: "estudiar node js y express",
+//     date: "2024-03-14T17:30:31.098Z",
+//     important: true,
+//   },
+//   {
+//     id: 3,
+//     content: "estudiar nest js y MongoDb",
+//     date: "2024-03-14T17:30:31.098Z",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "estudiar websockets",
+//     date: "2024-03-14T17:30:31.098Z",
+//     important: true,
+//   },
+// ];
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World </h1>");
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
@@ -77,7 +79,7 @@ app.post("/api/notes", (req, res) => {
   res.status(201).json(newNote);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
